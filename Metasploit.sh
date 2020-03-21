b4ac190c8d2e730293fbf16b443524b349defc80
Vatsal850 <53715295+Vatsal850@users.noreply.github.com>
2020-03-21 09:26:23 +0530

    Update Metasploit.sh

--- Metasploit.sh
+++ Metasploit.sh
@@ -1,5 +1,8 @@
-# apt-get update -y
-# apt install figlet -y
-# clear && figlet termux
-# apt install unstable-repo -y
-# apt install metasploit -y
+ apt-get update -y
+ apt upgrade -y
+ apt install figlet -y
+ clear && figlet termux
+ apt install unstable-repo -y
+ apt install metasploit -y
+ clear && echo "now you can start metasploit 
+   by typing 'msfconsole' "
